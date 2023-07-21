c15a26fd4994352390cbb689bdde47a5cd2f8196
Siddhartha Roy <sid86harth@gmail.com>
2023-07-22 03:05:00 +0530

    json fix

--- app/routes/pdf.py
+++ app/routes/pdf.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException
 from app.lib.utils import extract_images_from_pdf
 from pydantic import BaseModel
 from fastapi.responses import JSONResponse
-import json
+from fastapi.encoders import jsonable_encoder
 
 router = APIRouter()
 
@@ -14,19 +14,18 @@ class PDFBase64(BaseModel):
 @router.post("/extract/images")
 async def pdf_extract_images_stream(PdfBase64: PDFBase64):
 
-    print(PdfBase64)
-
+    # parse the request body
     try:
-        data = json.loads(PdfBase64)
+        data = jsonable_encoder(PdfBase64)
     except:
         raise HTTPException(status_code=400, detail="No PDF base64 provided")
 
     # Check if the base64 string is provided
-    if not data.base64:
+    if not data['base64']:
         raise HTTPException(status_code=400, detail="No PDF base64 provided")
 
     # Extract images from PDF
-    images = extract_images_from_pdf(data.base64)
+    images = extract_images_from_pdf(data['base64'])
 
     response = JSONResponse(content={"images": images, "total": len(images)})
 
